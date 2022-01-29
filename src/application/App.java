c7e00ffd05c4ad02eb1030c1375475e3bec73052
earl-stephens <stephensearl@hotmail.com>
2022-01-28 23:46:18 -0700

    Create notes for access modifiers

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,35 @@
 package application;
 
+import package1.Person;
+import package2.Employee;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* Modifier		Class	Package 	Subclass	World
+		 * public		Y		Y			Y			Y
+		 * protected 	Y		Y			Y			N
+		 * no modifier	Y		Y			N			N
+		 * private		Y		N			N			N
+		 */
+		
+		Person person1 = new Person("Joe");
+		Employee person2 = new Employee("Jane");
+		//System.out.println(person1.name);
+		//since Person name is public, can refer to it outside
+		//class and package
+		// If Person name was set to private, we could only refer
+		// it within the Person class, nowhere else
+		// If Person name was protected, we could only refer to it
+		// in the same package (package1 in this case).  So the Person
+		// class, any subclasses of Person, and any classes in package1
+		// could access it.
+		// If there is no modifier present, you can only refer to 
+		// in the class or same package - can't access it in a 
+		//subclass in a different package.
+		
+		// Rule is to make variables/methods as restricted as possible
+		// - encapsulation
 	}
 
 }
---
+++ src/package1/Person.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Person {
+	//can refer to name anywhere in the class
+	protected String name;
+	
+	public Person(String name) {
+		this.name = name;
+	}
+}
